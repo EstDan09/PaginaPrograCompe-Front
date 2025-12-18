649f036f5414355cf34576e91ed0b87996b90bed
Esteban Daniel Secaida Vargas <esecaidavargas@gmail.com>
2025-12-18 17:24:20 -0600

    [FEAT] - login basic rotuing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,10 @@ export const routes: Routes = [
         path: 'user',
         loadChildren:() =>
             import('./modules/user/user.routes').then(m => m.User_Routes)
+    },
+    {
+        path: 'auth',
+        loadChildren:() =>
+            import('./modules/auth/auth.routes').then(m => m.Auth_Routes)
     }
 ];
---
+++ src/app/modules/auth/auth.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { Login } from "./pages/login/login";
+
+export const Auth_Routes: Routes = [
+    {
+        path: '',
+        component: Login
+    }
+];
