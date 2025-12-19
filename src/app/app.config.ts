3e315b7803fda2b20de68f59b71cc949bc2c6915
Esteban Daniel Secaida Vargas <esecaidavargas@gmail.com>
2025-12-18 18:36:12 -0600

    [FEAT] - Basic UI
    needs work, just placeholder, check responsiveness

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from "@angular/common/http";
+import { provideTranslateService } from "@ngx-translate/core";
+import { provideTranslateHttpLoader } from "@ngx-translate/http-loader";
+
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: "assets/i18n/",
+        suffix: ".json",
+      }),
+      fallbackLang: "en",
+      lang: "es",
+    }),
   ]
 };
--- src/app/modules/auth/pages/login/login.ts
+++ src/app/modules/auth/pages/login/login.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslatePipe, TranslateService } from "@ngx-translate/core";
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  private _translateService = inject(TranslateService);
 
+  setLang(lang: "es" | "en") {
+    this._translateService.use(lang);
+  }
 }
