dd5b172e73685e83b6605462ed501df636fb5a98
Emanuel Rojas <emanuelrojas@Emanuels-MacBook-Air.local>
2025-12-20 22:41:07 -0600

    Created group service and component

---
+++ src/app/modules/user/pages/my-groups/my-groups.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-groups',
+  imports: [],
+  templateUrl: './my-groups.html',
+  styleUrl: './my-groups.scss',
+})
+export class MyGroups {
+
+}
