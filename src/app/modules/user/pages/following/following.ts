b5c4a2f43d55628129fd840144626be081ee0594
Emanuel Rojas <emanuelrojas@Emanuels-MacBook-Air.local>
2026-01-09 14:25:04 -0600

    [FEAT] Finnished following

--- src/app/modules/user/pages/following/following.ts
+++ src/app/modules/user/pages/following/following.ts
@@ -1,25 +1,33 @@
 import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { DatePipe } from '@angular/common';
-import { TranslatePipe } from '@ngx-translate/core';
-import { IFollowingList } from '../../../../models/following-list.model';
+import { TranslatePipe, TranslateService, TranslateModule } from '@ngx-translate/core';
+import { IFollowingList, IFollow } from '../../../../models/following-list.model';
 import { FollowingService } from '../../../../services/following.service';
 
 @Component({
   selector: 'app-following',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './following.html',
   styleUrl: './following.scss',
 })
 export class Following {
   private readonly changeDetectorRef = inject(ChangeDetectorRef);
   private _followingService: FollowingService = inject(FollowingService);
-  following: IFollowingList | undefined;
+  private _translateService = inject(TranslateService);
+  following: IFollow[] | undefined;
+  test: string[] = ["hola", "fuck", "you"];
+
+  setLang(lang: "es" | "en") {
+    this._translateService.use(lang);
+  }
 
 
   constructor() {
     this._followingService.getFollowing().subscribe((following) => {
-      if(following) {
+      if (following) {
         this.following = following;
+        console.log(this.following);
+        this.changeDetectorRef.markForCheck();
       }
       else {
         console.log("FAILED TO LOAD FRIENDS");
@@ -27,7 +35,5 @@ export class Following {
 
     });
   }
-
-
 
 }
--- src/app/services/following.service.ts
+++ src/app/services/following.service.ts
@@ -1,21 +1,21 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { IFollowingList } from '../models/following-list.model';
+import { IFollowingList, IFollow } from '../models/following-list.model';
 import { catchError, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FollowingService {
-  private _URL = `${environment.apiUrl}/following`
+  private _URL = `${environment.apiUrl}/following/get`
   private _http = inject(HttpClient)
 
-  private _followingList = signal<IFollowingList | null>(null);
+  private _followingList = signal<IFollow[] | null>(null);
 
 
   getFollowing() {
-    return this._http.get<IFollowingList>(this._URL).pipe(
+    return this._http.get<IFollow[]>(this._URL).pipe(
       tap(following => this._followingList.set(following)),
       catchError(() => {
         return of(null)
@@ -23,13 +23,6 @@ export class FollowingService {
     );
   }
 
-  getFollowingNames(following: IFollowingList | null) {
-    if (!following) {
-      console.log("There is no friend list");
-      return;
-    }
 
-
-  }
 
 }
