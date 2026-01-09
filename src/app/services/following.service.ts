8406e97253ee6128d96d0854e57a3165626db62e
Emanuel Rojas <emanuelrojas@Emanuels-MacBook-Air.local>
2026-01-08 19:09:40 -0600

    [FIX] fixed service

--- src/app/services/following.service.ts
+++ src/app/services/following.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FollowingService {
-  URL = 'http://localhost:3000/following/get'
-  async getFollowing() {
-    return await fetch(this.URL, {
-      credentials: 'include',
-    });
+  private _URL = `${environment.apiUrl}/following`
+  private _http = inject(HttpClient)
+
+
+
+
+  getFollowing() {
+    return this._http.get(this._URL);
   }
 
 }
